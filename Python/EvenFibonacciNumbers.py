6a8817a2ec5733fc19b96f4c8c9e5fc818da4fe7
mishanil <mishanilkazreen@gmail.com>
2025-10-20 11:19:05 +0100

    Removed sum function, as there is a native function and updating syntax to be more readable

--- Python/EvenFibonacciNumbers.py
+++ Python/EvenFibonacciNumbers.py
@@ -1,23 +1,15 @@
-def CheckEven(number):
+def check_even(number):
 
     if number % 2 == 0:
         return True
 
     return False
 
-def FilterEven(list):
+def filter_even(list):
 
-    return [n for n in list if CheckEven(n)]
+    return [n for n in list if check_even(n)]
 
-def CalculateSumOfList(list):
-    total = 0
-
-    for i in list:
-        total = total + i
-
-    return total
-
-def FibonacciSequence(list, base_case):
+def fibonacci_sequenece(list, base_case):
 
     next_number = list[::-1][0] + list[::-1][1]
 
@@ -26,11 +18,11 @@ def FibonacciSequence(list, base_case):
     else:
         return list
 
-    return FibonacciSequence(list, base_case)
+    return fibonacci_sequenece(list, base_case)
 
 if __name__ == "__main__":
-    Sequence = FibonacciSequence([1,2], 4000000)
-    EvenNumbers = FilterEven(Sequence)
-    Total = CalculateSumOfList(EvenNumbers)
+    sequence = fibonacci_sequenece([1,2], 4000000)
+    even_numbers = filter_even(sequence)
+    total = sum(even_numbers)
 
-    print(Total)
+    print(total)
